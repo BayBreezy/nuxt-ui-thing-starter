eee59a37a3f992391f38139115d2d291f4ea5a73
BayBreezy <behon.baker@yahoo.com>
2024-02-02 05:39:45 -0500

    chore(components): update to latest structure of components

--- components/Ui/GradientDivider.vue
+++ components/Ui/GradientDivider.vue
@@ -1,11 +1,24 @@
 <template>
-  <hr :class="styles({ class: props.class })" />
+  <Separator v-bind="reactiveOmit(props, 'class')" :class="styles({ class: props.class })" />
 </template>
 
 <script lang="ts" setup>
-  const props = defineProps<{ class?: any }>();
+  import { Separator } from "radix-vue";
+  import type { SeparatorProps } from "radix-vue";
+
+  const props = withDefaults(
+    defineProps<
+      SeparatorProps & {
+        /** Custom class(es) to add to parent element */
+        class?: any;
+      }
+    >(),
+    {
+      as: "hr",
+    }
+  );
 
   const styles = tv({
-    base: 'h-px border-t-0 bg-[linear-gradient(90deg,theme("colors.input/10%"),_theme("colors.input"),_theme("colors.input/10%"))]',
+    base: 'h-px w-full border-t-0 bg-[linear-gradient(90deg,theme("colors.input/10%"),_theme("colors.input"),_theme("colors.input/10%"))]',
   });
 </script>
