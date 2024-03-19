6257101b7a40ac2f03fef7fee4778e97955f7c36
BayBreezy <behon.baker@yahoo.com>
2024-03-19 05:59:54 -0500

    chore: wrap app in config provider to prevent some hydration warnings

--- app.vue
+++ app.vue
@@ -1,12 +1,20 @@
 <template>
-  <div>
-    <NuxtLoadingIndicator />
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
-    <!-- Delete the toast that you don't need -->
-    <UiToastToaster />
-    <UiVueSonner />
-    <UiNotivue />
-  </div>
+  <ConfigProvider :use-id="useIdFunction">
+    <div>
+      <NuxtLoadingIndicator />
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+      <!-- Delete the toast that you don't need -->
+      <UiToastToaster />
+      <UiVueSonner />
+      <UiNotivue />
+    </div>
+  </ConfigProvider>
 </template>
+
+<script setup lang="ts">
+  import { ConfigProvider } from "radix-vue";
+
+  const useIdFunction = () => useId();
+</script>
